51af453955310d3da8089b5f008b0bc618e17775
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-10 09:49:41 -0600

    Add recommend_exhibits method

--- lib/museum.rb
+++ lib/museum.rb
@@ -9,4 +9,16 @@ attr_reader :name,
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    result = []
+    @exhibits.each do |exhibit|
+      patron.interests.each do |interest|
+        if exhibit.name == interest
+          result << exhibit
+        end
+      end
+    end
+    result
+  end
 end
