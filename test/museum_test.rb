d3836185ba7988a2700e1c813d8308233e7108e7
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-10 09:16:50 -0600

    Add test for Museum class and instance

---
+++ test/museum_test.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/exhibit"
+require "./lib/patron"
+require "./lib/museum"
+
+class MuseumTest < Minitest::Test
+
+  def test_it_exists
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    assert_instance_of Museum, dmns
+  end
+
+end
